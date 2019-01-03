a552e2eee99f224c7efb589e7ef94ed194caefb7
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2019-01-03 14:13:07 -0500

    a little more haskell and notes

--- baby.hs
+++ baby.hs
@@ -1,6 +1,15 @@
+-- to start ghci type ghci in terminal
+-- then type :set prompt "ghci> " to make it nicer to work with
+-- to run script and load modules, type :l <script name>
+-- then you can call those functions from the command line with args
+
 doubleMe x = x + x
+
 doubleUs x y = x*2 + y*2
+
 doubleSmallNumber x = if x > 100 
                       then x 
                       else x*2
-doubleUs x y = doubleMe x + doubleMe y
+                      
+doubleUs' x y = doubleMe x + doubleMe y
+
